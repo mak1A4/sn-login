a17d5399cf9260a7b22a06a67b9aa8da20910a72
Martin Kirchweger <m.kirchweger@protonmail.com>
2022-03-29 10:49:51 +0200

    bump

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { AxiosInstance } from 'axios';
-import { CookieJar } from 'tough-cookie';
-interface loginReturnType {
-    token: string;
-    cookieJar: CookieJar;
-    wclient: AxiosInstance;
-}
-export default function (instance?: string, user?: string, pass?: string): Promise<loginReturnType>;
-export {};

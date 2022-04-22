6d63c23c8aa9105fac357804e6ec66a2987d6739
Martin Kirchweger <m.kirchweger@protonmail.com>
2022-04-22 18:54:36 +0200

    bump

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,8 +3,8 @@ import { CookieJar } from 'tough-cookie';
 export interface LoginData {
     token: string;
     wclient: AxiosInstance;
-    cookieJar?: CookieJar;
+    cookieJar: CookieJar;
 }
-declare function login(instance: string, user: string): Promise<LoginData>;
-declare function login(instance: string, user: string, pass?: string): Promise<LoginData>;
+declare function login(instance: string, user: string, mfa?: string): Promise<LoginData>;
+declare function login(instance: string, user: string, pass?: string, mfa?: string): Promise<LoginData>;
 export default login;
--- dist/index.js
+++ dist/index.js
@@ -36,24 +36,57 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+var os = require("os");
+var fs = require("fs");
+var path = require("path");
+var crypto = require("crypto");
 var axios_1 = require("axios");
 var tough_cookie_1 = require("tough-cookie");
 var axios_cookiejar_support_1 = require("axios-cookiejar-support");
 var url_1 = require("url");
 var keytar_1 = require("keytar");
-function login(instance, user, pass) {
+function encrypt(text, key) {
+    var iv = crypto.randomBytes(16);
+    var keyHash = crypto.createHash('sha256').update(String(key)).digest("hex");
+    var cipher = crypto.createCipheriv("aes-256-ctr", Buffer.from(keyHash, "hex"), iv);
+    var encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return iv.toString("hex") + ':' + encrypted.toString("hex");
+}
+function decrypt(text, key) {
+    var textParts = text.split(":");
+    var iv = Buffer.from(textParts.shift(), "hex");
+    var encryptedText = Buffer.from(textParts.join(":"), "hex");
+    var keyHash = crypto.createHash('sha256').update(String(key)).digest("hex");
+    var decipher = crypto.createDecipheriv("aes-256-ctr", Buffer.from(keyHash, "hex"), iv);
+    var decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
+function testLogin(snClient, instance) {
     return __awaiter(this, void 0, void 0, function () {
-        var INSTANCE_NAME, jar, instanceURL, snClient, userPassword, password, loginFormData, loginResponse, responseBody, ck;
+        var response;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, snClient.get("/stats.do")];
+                case 1:
+                    response = _a.sent();
+                    return [2 /*return*/, response.data.toString().indexOf("Instance name: " + instance) >= 0];
+            }
+        });
+    });
+}
+function login(instance, user, pass, mfa) {
+    return __awaiter(this, void 0, void 0, function () {
+        var INSTANCE_NAME, instanceURL, userPassword, password, jar, cookieFilePath, encryptedCookieStr, decryptedCookieStr, cookieObj, wclient, snClient, loginFormData, loginResponse, responseBody, ck, loginSuccessful, cookieObjStr, encryptedCookieObj;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
                     INSTANCE_NAME = "".concat(instance, ".service-now.com");
-                    jar = new tough_cookie_1.CookieJar();
                     instanceURL = "https://".concat(INSTANCE_NAME);
-                    snClient = (0, axios_cookiejar_support_1.wrapper)(axios_1.default.create({ jar: jar, baseURL: instanceURL }));
                     userPassword = pass;
                     if (!!userPassword) return [3 /*break*/, 2];
-                    return [4 /*yield*/, (0, keytar_1.getPassword)(instance, user)];
+                    return [4 /*yield*/, (0, keytar_1.getPassword)(INSTANCE_NAME, user)];
                 case 1:
                     password = _a.sent();
                     if (password)
@@ -62,6 +95,31 @@ function login(instance, user, pass) {
                         throw "Couldn't find user password";
                     _a.label = 2;
                 case 2:
+                    jar = new tough_cookie_1.CookieJar();
+                    cookieFilePath = path.join(os.tmpdir(), "".concat(instance, ":").concat(user, "_cookie.json"));
+                    if (!fs.existsSync(cookieFilePath)) return [3 /*break*/, 4];
+                    encryptedCookieStr = fs.readFileSync(cookieFilePath, 'utf8');
+                    decryptedCookieStr = decrypt(encryptedCookieStr, userPassword);
+                    cookieObj = JSON.parse(decryptedCookieStr);
+                    cookieObj.cookieJar.cookies.forEach(function (c) {
+                        var cookie = new tough_cookie_1.Cookie(c);
+                        jar.setCookieSync(cookie, instanceURL);
+                    });
+                    wclient = (0, axios_cookiejar_support_1.wrapper)(axios_1.default.create({ jar: jar, baseURL: instanceURL }));
+                    return [4 /*yield*/, testLogin(wclient, instance)];
+                case 3:
+                    if (_a.sent()) {
+                        return [2 /*return*/, {
+                                "token": cookieObj.token,
+                                "cookieJar": cookieObj.cookieJar,
+                                "wclient": wclient
+                            }];
+                    }
+                    _a.label = 4;
+                case 4:
+                    snClient = (0, axios_cookiejar_support_1.wrapper)(axios_1.default.create({ jar: jar, baseURL: instanceURL }));
+                    if (mfa)
+                        userPassword += mfa;
                     loginFormData = new url_1.URLSearchParams({
                         "user_name": user, "user_password": userPassword,
                         "remember_me": "true", "sys_action": "sysverb_login"
@@ -73,11 +131,22 @@ function login(instance, user, pass) {
                                 "Content-Type": "application/x-www-form-urlencoded"
                             }
                         })];
-                case 3:
+                case 5:
                     loginResponse = _a.sent();
                     responseBody = loginResponse.data;
                     ck = responseBody.split("var g_ck = '")[1].split('\'')[0];
                     snClient.defaults.headers.common["X-UserToken"] = ck;
+                    return [4 /*yield*/, testLogin(snClient, instance)];
+                case 6:
+                    loginSuccessful = _a.sent();
+                    if (!loginSuccessful)
+                        throw "Login failed, MFA required?";
+                    cookieObjStr = JSON.stringify({
+                        "cookieJar": jar.toJSON(),
+                        "token": ck
+                    });
+                    encryptedCookieObj = encrypt(cookieObjStr, userPassword);
+                    fs.writeFileSync(cookieFilePath, encryptedCookieObj, "utf-8");
                     return [2 /*return*/, {
                             "token": ck,
                             "cookieJar": jar,
